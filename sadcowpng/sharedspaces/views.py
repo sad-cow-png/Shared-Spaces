b921c462b0b1aa381c5f73a305107ff61ed69b59
Sharlet Claros <sharlet.claros@outlook.com>
2021-03-10 23:37:22 -0500

    Linked HTML pages to project URL paths. Shout out to Binh for crucial catch.

--- sadcowpng/sharedspaces/views.py
+++ sadcowpng/sharedspaces/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
-
+from django.template import loader
 from django.http import HttpResponse
 
 # Shared Spaces Home Page
 def index(request):
-    return HttpResponse("Welcome to Shared Spaces")
+    return render(request, 'sharedspaces/index.html')
+
 
 # account page renders based on user input role
 def account(request):
-    return HttpResponse("Here is your account page!")
+    return render(request, 'sharedspaces/account.html')
+
 
 def login(request):
-    return HttpResponse("Login here")
+    return render(request, 'sharedspaces/login.html')
+
 
 def sign_up(request):
-    return HttpResponse("Sign up here")
+    return render(request, 'sharedspaces/signup.html')
+
 
 def create_space(request):
-    return HttpResponse("Create your space here!")
+    return render(request, 'sharedspaces/create_space.html')
