737e64134f33c4f66116ea501ccca4b8920b3e4f
bt731 <62581040+bt731@users.noreply.github.com>
2021-03-26 15:02:52 -0400

    Added base model for user account creation

--- sadcowpng/sharedspaces/models.py
+++ sadcowpng/sharedspaces/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# Create user with either as a client or proprietor
+class User(AbstractUser):
+    is_client = models.BooleanField(default=False)
+    is_proprietor = models.BooleanField(default=False)
+
+
+
+
