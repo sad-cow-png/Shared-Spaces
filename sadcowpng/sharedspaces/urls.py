98cd328f7381404868dfab55d1726771e924398b
btruong9 <62581040+bt731@users.noreply.github.com>
2021-03-29 19:47:50 -0400

    Updated urls and added logic to account template

--- sadcowpng/sharedspaces/urls.py
+++ sadcowpng/sharedspaces/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('account/', views.account, name='account'),
     path('login/', views.login, name='login'),
+    path('login/proprietor/', views.proprietor_login_view, name='proprietor_login'),
     path('sign_up/', views.sign_up, name='sign_up'),
-    path('sign_up/proprietor/', views.ProprietorSignUpView.as_view(), name='proprietor_sign_up'),
+    path('sign_up/proprietor/', views.proprietor_sign_up_view, name='proprietor_sign_up'),
     path('create/', views.create_space, name='create_space'),
 
 ]
