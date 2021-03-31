ad614319f8e6c02362ba2f52afd487f9808af580
btruong9 <62581040+bt731@users.noreply.github.com>
2021-03-30 21:19:59 -0400

    Wrote tests for signup and login

--- sadcowpng/sharedspaces/tests.py
+++ sadcowpng/sharedspaces/tests.py
@@ -1,3 +1,129 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import User
+from .forms import ProprietorSignUpForm
+
+
+class ProprietorSignUpFormTest(TestCase):
+    def setUp(self):
+        # Used for existing user error
+        self.credentials = {
+            'username': 'test',
+            'password': '#zgsXJLY5jRb35j',
+        }
+        User.objects.create_user(**self.credentials)
+        self.assertTrue(User.objects.count(), 1)
+
+    def test_signup_page(self):
+        response = self.client.get('/sign_up/proprietor/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='sharedspaces/proprietor_signup.html')
+
+    def test_signup_view(self):
+        response = self.client.get(reverse('proprietor_sign_up'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='sharedspaces/proprietor_signup.html')
+
+    def test_invalid_signup(self):
+        invalid_data = [
+            # username already exists
+            {
+                'data': {
+                    'username': 'test',
+                    'password1': '#zgsXJLY5jRb35j',
+                    'password2': '#zgsXJLY5jRb35j',
+                }
+            },
+            # password entirely numeric
+            {
+                'data': {
+                    'username': 'john',
+                    'password1': '1233556',
+                    'password2': '1233556'
+                }
+            },
+            # password too short
+            {
+                'data': {
+                    'username': 'someone',
+                    'password1': 'sdfg34',
+                    'password2': 'sdfg34',
+                }
+            },
+
+            # password too common
+            {
+                'data': {
+                    'username': 'foo',
+                    'password1': 'asdfghjkl',
+                    'password2': 'asdfghjkl',
+                }
+            },
+
+            # password doesn't match
+            {
+                'data': {
+                    'username': 'foo2',
+                    'password1': 'XhajJSkd45',
+                    'password2': 'jahkHAWO34g',
+                }
+            },
+
+        ]
+        for invalid_users in invalid_data:
+            form = ProprietorSignUpForm(data=invalid_users['data'])
+            self.failIf(form.is_valid())
+
+
+    def test_valid_signup(self):
+        data = {
+            'username': 'testuser2',
+            'password1': 'XGAHvnd457',
+            'password2': 'XGAHvnd457',
+        }
+
+        # Check form has correct fields
+        response = self.client.get('/sign_up/proprietor/')
+        self.assertContains(response, 'username')
+        self.assertContains(response, 'password1')
+        self.assertContains(response, 'password2')
+
+        # Sign up with user data
+        response = self.client.post('/sign_up/proprietor/', data=data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(User.objects.count(), 2)
+
+        self.assertRedirects(response, '/')
+
+
+# Test proprietor login form and view
+class ProprietorLoginTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': '#zgsXJLY5jRb35j',
+        }
+        User.objects.create_user(**self.credentials)
+        user = User.objects.all()
+        self.assertTrue(user.count(), 1)
+
+    def test_login_view(self):
+        response = self.client.get('/login/proprietor/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='sharedspaces/proprietor_login.html')
+
+    def test_login_fields(self):
+        response = self.client.get('/login/proprietor/')
+        self.assertContains(response, 'username')
+        self.assertContains(response, 'password')
+
+    def test_login(self):
+        response = self.client.post('/login/proprietor/', self.credentials, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if user is logged in
+        self.assertTrue(response.context['user'].is_authenticated)
+        self.assertTrue(response.context['user'].is_active)
+
+        self.assertRedirects(response, '/account/')
