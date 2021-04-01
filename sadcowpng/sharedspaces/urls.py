cbe3b381e957605f3aaa73960aa06d3f694d58c0
Jimmy Coleman <jcolema3@umbc.edu>
2021-04-01 01:09:03 -0400

    Fix error with url path naming.

--- sadcowpng/sharedspaces/urls.py
+++ sadcowpng/sharedspaces/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('account/', views.account, name='account'),
     path('login/', views.login, name='login'),
     path('sign_up/', views.sign_up, name='sign_up'),
-    path('create_space/', views.create_space, name='create-space'),
-    path('update_space/<space_id>', views.update_space, name='update-space'),
+    path('create_space/', views.create_space, name='create_space'),
+    path('update_space/<space_id>', views.update_space, name='update_space'),
 
 
 
